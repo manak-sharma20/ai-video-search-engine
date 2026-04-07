7e9c22787966e72c005ccc6afb007f802757b994
Kumar manak <manakkash20@gmail.com>
2026-04-07 16:46:38 +0530

    feat: add audio transcription with Whisper and timestamp-based video search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
-
 import chromadb
+import whisper
+
 model=SentenceTransformer("all-MiniLM-L6-v2")
 sentences=["I love football","I enjoy soccer"]
 embedding=model.encode(sentences)
@@ -11,10 +12,31 @@ collection.add(
     embeddings=embedding,
     ids=["1","2"]
 )
-query="football match"
+query="creation"
 query_embedding=model.encode(query)
 result=collection.query(
     query_embeddings=[query_embedding],
     n_results=1
 )
-print(result)
+whisper_model=whisper.load_model("base")
+result=whisper_model.transcribe("video.mp4")
+segments=result["segments"]
+i=0
+for segment in segments:
+    text=segment["text"]
+    start=segment["start"]
+    emb=model.encode(text)
+    collection.add(
+        documents=[text],
+        embeddings=[emb],
+        ids=[str(i)],
+        metadatas=[{"start": start}]
+    )
+    i+=1
+results = collection.query(
+    query_embeddings=[query_embedding],
+    n_results=2
+)
+
+print(results["documents"])
+print(results["metadatas"])
